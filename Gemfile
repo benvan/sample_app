5c02554ecbae4a8db77b83b48926d31256bf69c5
Benjamin van Enckevort <benvan+git@gmail.com>
2010-09-05 17:28:28 +0200

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.0.rc'
+gem 'sqlite3-ruby', :require => 'sqlite3'
+
+# Bundle gems for the local environment. Make sure to
+# put test-only gems in this group so their generators
+# and rake tasks are available in development mode:
+group :development do
+      gem 'rspec-rails', '2.0.0.beta.18'
+end
+
+group :test do
+      gem 'rspec', '2.0.0.beta.18'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'f7bd5a6ee08c83a6f4e9663d31121101389182ff7da02d465e1e5df2506a06605ea0227ba46876ac5fb96861aaf1ff0bddd0b888b31e43fdc4e43a46f35e1cac'
